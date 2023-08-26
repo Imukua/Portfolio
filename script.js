12856ee11a055ca0cd97b9f77f796f11945b8439
Imukua <ianmukuaa@gmail.com>
2023-08-26 10:25:20 +0300

    added last name switch animation

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+
+const lastName = document.querySelector('.last-name');
+
+lastName.addEventListener('mouseenter', () => {
+  lastName.textContent = 'Okello';
+  lastName.style.color = 'red'; // Change the color if needed
+});
+
+lastName.addEventListener('mouseleave', () => {
+  lastName.textContent = 'Mukua';
+  lastName.style.color = ''; // Reset the color
+});
